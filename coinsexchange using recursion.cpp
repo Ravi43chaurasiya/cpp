f8652d21cbf762f1284ba5c4087de4033e6a4721
Ravi43chaurasiya <67574804+Ravi43chaurasiya@users.noreply.github.com>
2021-04-23 10:34:07 +0530

    Add files via upload

---
+++ coinsexchange using recursion.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+int coinsexchange(int amt,int *dino,int n)
+{
+	// base case
+	if(amt==0)
+	{
+		return 0;
+	}
+	// recursive case
+	int ans=INT_MAX;
+	for(int i=0;i<n;i++)
+	{
+		if(amt>=dino[i])
+		{
+			int sa=coinsexchange(amt-dino[i],dino,n);
+			if(sa!=INT_MAX)
+			{
+				ans=min(sa+1,ans);
+			}
+		}
+	}
+	return ans;
+}
+
+
+
+int main()
+{
+	int dino[4]={1,7,10};
+	int amount;
+	cin>>amount;
+	cout<<coinsexchange(amount,dino,3);
+	return 0;
+}
